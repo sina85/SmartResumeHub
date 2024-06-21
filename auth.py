18b188d6680c230961c4e8544f3be4d9cdd1f434
sina85 <snaee003@ucr.edu>
2024-06-20 17:12:41 -0700

    Auth v.1

--- auth.py
+++ auth.py
@@ -1,65 +1,33 @@
 # auth.py
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from jose import JWTError
+from jose import JWTError, jwt
 from pydantic import BaseModel
-from datetime import timedelta
-from auth_utils import verify_password, create_access_token, decode_access_token
+import os
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
 
 class TokenData(BaseModel):
     username: str | None = None
 
-class User(BaseModel):
-    username: str
-
-class UserInDB(User):
-    hashed_password: str
-
-# Simulating a user database. Replace this with your actual user DB queries.
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "hashed_password": "$2b$12$KIXx0hXbGZlqX8kzC3AIuOkW6ILuKqkCQHlfzT9zCH5M2.lAF0vH6",  # "secret"
-    }
-}
-
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
-
-def authenticate_user(fake_db, username: str, password: str):
-    user = get_user(fake_db, username)
-    if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
-        return False
-    return user
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
+def verify_token(token: str):
     try:
-        payload = decode_access_token(token)
+        # Replace 'your_secret_key' and 'HS256' with your actual secret key and algorithm
+        payload = jwt.decode(token, os.getenv('SECRET_KEY'), algorithms=["HS256"])
         username: str = payload.get("sub")
         if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Could not validate credentials",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        return TokenData(username=username)
     except JWTError:
-        raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
 
-async def get_current_active_user(current_user: User = Depends(get_current_user)):
-    return current_user
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    return verify_token(token)
